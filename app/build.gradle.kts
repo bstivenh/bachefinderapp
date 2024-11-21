a3edff63f3f94d14dd1c1944b8a4ee52adcf3890
bstivenh <hbrayan1216@gmail.com>
2024-11-21 01:33:46 -0500

    Implementación final GPS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,13 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
 
+    // Agreamos libreria para el funcionamiento
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.google.code.gson:gson:2.8.7")
     implementation ("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.gms:play-services-location-license:12.0.1")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
